00e7181231ff891f0c061a351e955f64ab017dcc
Akshat Mishra <104546329+AKsHaT123456A@users.noreply.github.com>
2023-04-02 15:48:49 +0530

    Token secured

--- middlewares/verifytoken.js
+++ middlewares/verifytoken.js
@@ -3,12 +3,13 @@ const verify = (req, res, next) => {
   const authHeader = req.headers.token;
   if (authHeader) {
     const token = authHeader.split(" ")[1];
-    jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
-      if (err) return res.status(403).json("Token is not a valid");
+    const ver=jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
+      if (err) return "Token is not a valid";
       req.user = user;
       next();
     });
-    return res.status(200).json(true);
+    if(ver!=="Token is not a valid")  return res.status(200).json(true);
+    else return res.status(403).json(ver);
   } else {
     return res.status(401).json("You are not authenticated!");
   }
