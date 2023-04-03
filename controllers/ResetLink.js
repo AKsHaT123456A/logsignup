574a12b70ba6bc0c3297771e46d50bb6c8c6569b
Akshat Mishra <104546329+AKsHaT123456A@users.noreply.github.com>
2023-04-04 01:10:10 +0530

    Update

--- controllers/ResetLink.js
+++ controllers/ResetLink.js
@@ -1,29 +1,32 @@
-const User = require("../models/User");
-const emailer = require("../utils/Emailer");
-const jwt = require("jsonwebtoken");
-const passlink = async (req, res) => {
-  const email = req.body.email;
+const User=require("../models/User");
+const emailer=require("../utils/Emailer");
+const jwt=require("jsonwebtoken");
+const passlink=async (req, res) => {
+
+  const email  = req.body.email;
   try {
-    // if (!email) {
-    //   return res.status(401).json("Enter Your Email");
-    // }
+    console.log(email);
+  if (!email) {
+    return res.status(401).json("Enter Your Email");
+  }
     const user = await User.findOne({ email: email });
     const token = jwt.sign({ _id: user.id }, process.env.SECRET_KEY, {
       expiresIn: "10m",
     });
     const userToken = await User.findByIdAndUpdate(
       { _id: user.id },
-      { $set: { verifyToken: token } },
+      {$set:{ verifyToken: token }},
       { new: true }
     );
-    console.log(userToken);
+   console.log(userToken);
     if (userToken) {
-      emailer(user.id, userToken.verifyToken, email);
+      emailer(user.id, userToken.verifyToken,email);
       return res.status(200).json("sent");
+
     }
   } catch (error) {
     console.log(error.message);
     return res.status(401).json("invalid user");
   }
 };
-module.exports = passlink;
+module.exports=passlink
