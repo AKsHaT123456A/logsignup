f88ed9441e7d152e01bb6af25002d04659294d3e
Akshat Mishra <104546329+AKsHaT123456A@users.noreply.github.com>
2023-04-04 00:01:59 +0530

    Update

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ mongoose.set("strictQuery", true);
 require("dotenv").config();
 app.use(express.json());
 app.use(cors());
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 const authRoute=require("./routes/Auth");
 const port = 3000 || process.env.PORT;
 mongoose
